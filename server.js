1e79f5d91a65443aabe0d01ba563c71b94308d0e
Tiến Nguyễn Nhật <nhattien.nguyen@upskills.com>
2025-11-28 23:36:30 +0700

    add folder public for CSS

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("notindex");
 });
 
-app.listen(6969);
+app.listen(6969, () => {
+  console.log("Server running on http://localhost:6969");
+});
